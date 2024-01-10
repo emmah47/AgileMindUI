b48b93b2de0816342961764bf7d9f7899aec60af
Falconfeather4 <huangsimiao2014@gmail.com>
2024-01-10 12:20:37 -0800

    fixed the search and sort by

--- src/components/home/Projects.jsx
+++ src/components/home/Projects.jsx
@@ -81,15 +81,44 @@ function SearchBar( {onSearch} ) {
 }
 
 function ProjectsList( {projects, searchText, sortBy} ) {
+  // filter by search text
   let filteredProjectsList = projects.filter((project) => {
       return project.name.toLowerCase().includes(searchText.toLowerCase());
   });
 
+  // sort projects based on sortBy
+  let SortedProjectsList;
+  switch (sortBy) {
+    case "by-recent":
+      SortedProjectsList = filteredProjectsList.sort((p1, p2) => {
+        // fsr projects is empty when this runs for the first time so we have to check if its null
+        const date1 = p1?.lastOpenedDate ? new Date(p1.lastOpenedDate).getTime() : 0;
+        const date2 = p2?.lastOpenedDate ? new Date(p2.lastOpenedDate).getTime() : 0;
+        return date2 - date1;
+      });
+      break;
+    case "by-name":
+      SortedProjectsList = filteredProjectsList.sort((p1, p2) => {
+        const name1 = p1?.name ? p1.name : "";
+        const name2 = p2?.name ? p2.name : "";
+        return name1.localeCompare(name2);
+      });
+      break;
+    case "creation-time":
+      SortedProjectsList = filteredProjectsList.sort((p1, p2) => {
+        const date1 = p1?.creationDate ? new Date(p1.creationDate).getTime() : 0;
+        const date2 = p2?.creationDate ? new Date(p2.creationDate).getTime() : 0;
+        return date2 - date1;
+      });
+      break;
+  }
 
-  let filteredProjectsComponentsList = projects.map((project) => {
+  // make each project into a component and put it in a list
+  let filteredProjectsComponentsList = SortedProjectsList.map((project) => {
     return (<div key={project.id}><Project project={project} /></div>);
   });
 
+  // put the add project button at the front of the list
   filteredProjectsComponentsList.unshift(<div key="add-project-button"><AddProjectButton /></div>)
 
   return (
